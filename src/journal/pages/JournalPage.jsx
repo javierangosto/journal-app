f774fc7450b56b14bbdb5e82e0848683482488ef
Javier Angosto <javier.angosto@gmail.com>
2023-03-24 23:51:49 +0100

    Layout Journal created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,12 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout"
 
 export const JournalPage = () => {
     return (
         
-        <>
-            <Typography variant='h1'>JournalPage</Typography>
-            <hr />
-        </>
+        <JournalLayout>
+            <Typography>Consectetur reprehenderit voluptate sint ex ad velit qui veniam esse voluptate est. Consectetur laboris duis id labore aliquip tempor ipsum ea irure eu do pariatur duis. Magna commodo exercitation cillum aliquip reprehenderit eu incididunt fugiat non amet excepteur. Veniam ut laboris cupidatat non mollit amet anim esse qui consectetur veniam sunt.</Typography>
+        </JournalLayout>
 
     )
 }
