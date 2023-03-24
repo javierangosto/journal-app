ede2b4441dff243a2718a74108e0ececec088c98
Javier Angosto <javier.angosto@gmail.com>
2023-03-24 22:57:00 +0100

    MUI installation and theme config

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { AppRouter } from './router'
+import { AppTheme } from './theme'
 
 export const JournalApp = () => {
     return (
         
-        <>
+        <AppTheme>
             <AppRouter />
-        </>
+        </AppTheme>
 
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <BrowserRouter>
       <JournalApp />
     </BrowserRouter>
+    
   </React.StrictMode>,
 )
