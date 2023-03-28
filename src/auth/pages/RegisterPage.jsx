5ef32db99fb59d8ce539a76b306ec296b6b8a606
Javier Angosto <javier.angosto@gmail.com>
2023-03-28 14:35:45 +0200

    Validate from form hook

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,36 @@
 import { Link as RouterLink } from 'react-router-dom'
-import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
+
+const formData = {
+    displayName: 'Javier Angosto',
+    email: 'jang@react.com',
+    password: '1234',
+}
+
+const formValidations = {
+    displayName: [( value ) => value.length >= 1, 'The name is required'],
+    email: [( value ) => value.includes('@'), 'The email address is invalid'],
+    password: [( value ) => value.length >= 6, 'The password must have more than six characters'],
+    
+}
 
 export const RegisterPage = () => {
+
+    const { 
+        formState, displayName, email, password, onInputChange,
+        isFormValid, displayNameValid, emailValid, passwordValid
+    } = useForm( formData, formValidations );
+
+    const handleSubmit = ( event ) => {
+        event.preventDefault();
+        console.log(formState)
+    }
+
     return (
         <AuthLayout title = 'Register'>
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField 
@@ -14,6 +38,11 @@ export const RegisterPage = () => {
                             type="name" 
                             placeholder="Your name"
                             fullWidth
+                            name="displayName"
+                            value={ displayName }
+                            onChange = { onInputChange }
+                            error={ !displayNameValid }
+                            helperText={ displayNameValid }
                         />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,6 +51,9 @@ export const RegisterPage = () => {
                             type="email" 
                             placeholder="email@email.com"
                             fullWidth
+                            name="email"
+                            value={ email }
+                            onChange = { onInputChange }
                         />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -30,6 +62,9 @@ export const RegisterPage = () => {
                             type="password" 
                             placeholder="Password"
                             fullWidth
+                            name="password"
+                            value={ password }
+                            onChange = { onInputChange }
                         />
                     </Grid>
                     <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }} >
@@ -37,6 +72,7 @@ export const RegisterPage = () => {
                             <Button
                                 variant='contained' 
                                 fullWidth
+                                type="submit"
                             >
                                 Create an account
                             </Button>
