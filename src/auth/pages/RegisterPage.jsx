3d3f3d7c96029e3391db20d6be88f1549fa3e601
Javier Angosto <javier.angosto@gmail.com>
2023-03-29 00:27:57 +0200

    Animations created

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -44,7 +44,10 @@ export const RegisterPage = () => {
 
     return (
         <AuthLayout title = 'Register'>
-            <form onSubmit={ handleSubmit }>
+            <form 
+                onSubmit={ handleSubmit }
+                className="animate__animated animate__fadeIn animate__faster"
+            >
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField 
